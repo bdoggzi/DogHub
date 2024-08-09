74dbf4d73997799348611b973721a1475062ccc2
bdoggzi <96205678+bdoggzi@users.noreply.github.com>
2024-08-09 22:50:11 +0100

    hub.lua

--- hub.lua
+++ hub.lua
@@ -1,22 +1,35 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Dog Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "DogHub"})
 
-local Tab = Window:MakeTab({
+local GameScripts = Window:MakeTab({
 	Name = "Game Scripts",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 
-Tab:AddButton({
+GameScripts:AddButton({
 	Name = "Sensation (Base Battles / Jailbreak)",
 	Callback = function()
       	loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/730854e5b6499ee91deb1080e8e12ae3.lua"))()
   	end    
 })
 
-Tab:AddButton({
+GameScripts:AddButton({
 	Name = "Eclipse.lol (Phantom Forces / Rivals",
 	Callback = function()
       	loadstring(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/loader.lua"))()
+  	end    
+})
+
+local MiscScripts = Window:MakeTab({
+	Name = "Misc Scripts",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+MiscScripts:AddButton({
+	Name = "Infinite Yield",
+	Callback = function()
+      	loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
   	end    
 })
